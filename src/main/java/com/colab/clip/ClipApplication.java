bee9f71a1fc06447b53cd363417bb1726151b1c2
nadun96 <nadunudaraka@gmail.com>
2024-01-02 15:49:56 +0530

    --init

---
+++ src/main/java/com/colab/clip/ClipApplication.java
@@ -0,0 +1,13 @@
+package com.colab.clip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipApplication.class, args);
+	}
+
+}
